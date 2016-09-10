3162c1c4c55f094516a71c478d49d12b99734c05
Sahil754 <sahilm754@gmail.com>
2016-09-09 20:10:26 -0400

    Initial commit
    No discription.

---
+++ Welcome1.java
@@ -0,0 +1,9 @@
+// This is class Welcome1
+public class Welcome1
+{      //Main entry point for the application
+       public static void main(String[] args)
+       {     //Display  a welcome message to the console
+             System.out.print("Welcome to ");
+             System.out.printIn("Java Programming!");
+       }       
+}
---
+++ Welcome3.java
@@ -0,0 +1,8 @@
+// This is class Welcome3
+public class Welcome3
+{      //Main entry point for the application
+       public static void main(String[] args)
+       {     //Display  a welcome message to the console
+             System.out.printf("%s%n%s"
+       }       
+}
